0023ce370e911dcac273e8729be60830d3622b89
Joshua Gonzalez <jalgonz96@gmail.com>
2021-11-08 11:16:24 -0500

    importing scss and fixing typo on navbar height

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import "./navbar.scss"
 
-export default function navbar() {
+export default function Navbar() {
     return (
         <div className="navbar">
             
