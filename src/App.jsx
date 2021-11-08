6280007fc1e87d9b1e590de52d2cc4252e89d31c
Joshua Gonzalez <jalgonz96@gmail.com>
2021-11-08 11:20:46 -0500

    importing all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import Contact from "./components/contact/Contact";
+import Intro from "./components/intro/Intro";
 import Navbar from "./components/navbar/Navbar";
-
+import Portfolio from "./components/portfolio/Portfolio";
+import Works from "./components/works/Works";
 
 function App() {
   return (
     <div className="app">
       <Navbar/>
-      <div className="sections"></div>
+      <div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Works/>
+        <Contact/>
+      </div>
     </div>
   );
 }
