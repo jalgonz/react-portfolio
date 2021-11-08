b2c7ef4963ffbc903a7f5a59720125c16e8b9582
Joshua Gonzalez <jalgonz96@gmail.com>
2021-11-08 16:41:13 -0500

    added style for hamburger menu transition

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Navbar from "./components/navbar/Navbar";
 import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import "./app.scss"
+import { useState } from "react";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <div className="app">
-      <Navbar/>
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
