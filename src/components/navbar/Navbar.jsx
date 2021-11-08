e0b0c47858bf7f185dbf4ed6a3497abe95506103
Joshua Gonzalez <jalgonz96@gmail.com>
2021-11-08 15:27:30 -0500

    added styling for navbar and section scrolling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,12 @@ import "./navbar.scss"
 export default function Navbar() {
     return (
         <div className="navbar">
-            
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">genius.</a>
+                </div>
+                <div className="right"></div>
+            </div>
         </div>
     )
 }
